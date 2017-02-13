0b37fcc0f4f620de50613533e68c40d1d66e2aa7
akplearner <akplearner@gmail.com>
2017-02-12 21:31:07 -0500

    v.02

---
+++ book/chapt2/count2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int i=1,n=0;
+	cout<<"Enter a number and press ENTER: ";
+	cin>>n;
+
+	while(i<=n) {
+	cout<<i<<"";
+	i=i+1
+	}
+	return 0;
+}
